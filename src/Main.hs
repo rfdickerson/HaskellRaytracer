17baeba264550cf42dc86fef3d3345c7591e7a4c
Robert F. Dickerson <rfdickerson@gmail.com>
2015-11-08 19:08:34 -0600

    added some PPM stuff

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
 import Data.List
+import Control.Concurrent
+import Control.Monad
+import Control.Concurrent.STM
 import qualified Data.Matrix as M
 import qualified Data.Vector as V
 
@@ -9,19 +12,33 @@ type Vector = M.Matrix RealNum
 type Point = Vector
 type Radius = RealNum
 type Transform = M.Matrix RealNum
+type Bitmap = [Pixel]
 
-data Ray = Ray Point Vector
+
+data Ray = Ray Point Vector deriving (Show, Eq)
 data Geometry = Sphere Radius Point | Triangle
 
+-- | Pixel represents a color, r, g, b
+data Pixel = RGB Float Float Float deriving (Show, Eq)
+data Image = Image Int Int [Pixel] deriving Show
+
 -- | Camera
--- Position, view direction, aspect ratio, up direction
-data Camera = Camera Point Vector RealNum Vector
+-- World to camera, cameraToWorld, clipHither, clipYon
+data Camera = Camera Transform Transform RealNum RealNum
   deriving (Show)
 
+--pi :: RealNum
+--pi = 3.14159265359
+
+
+     
 rayEpsilon :: RealNum
 rayEpsilon = 1e-7
 
 a = M.fromList 1 4 [1..4::Float]
+
+toRadians :: RealNum -> RealNum
+toRadians deg = deg * pi / 180.0
 
 -- | Vector length (norm)
 vlength :: Vector -> RealNum
@@ -47,16 +64,28 @@ maxRaySteps = 5
 newVector :: [RealNum] -> Point
 newVector x = M.fromList 4 1 x
 
-defaultCamera = Camera (newVector [5,0,0]) (newVector [-1,0,0]) 1.0 (newVector [0,1,0])
+defaultCamera = Camera (translateT $ newVector [5,0,0]) (translateT $ newVector [-5,0,0]) 0.010 100.0
 
 cameraTransform :: Camera -> Transform
-cameraTransform c = identityTransform
+cameraTransform (Camera w2c c2w hither yon) = M.fromList 4 4 [0,0,0,0,0,0,0,0,0,0,a,b,0,0,0,0] + identityTransform
+  where a = yon / (yon-hither)
+        b = -(yon*hither/(yon-hither))
+
+invTanAng :: RealNum -> RealNum
+invTanAng fov = 1.0/tan(toRadians(fov) / 2.0)
 
 -- defaultCamera :: Camera
 -- defaultCamera = Camera identityTransform 0.1 100 0 0
 
 identityTransform :: Transform
 identityTransform = M.identity 4
+
+scaleT :: Vector -> Transform
+scaleT v = M.fromList 4 4 [x,0,0,0,0,y,0,0,0,0,z,0,0,0,0,1]
+           where
+             x = M.getElem 1 1 v
+             y = M.getElem 2 1 v
+             z = M.getElem 3 1 v
 
 translateT :: Vector -> Transform
 translateT v = (M.fromList 4 4 t) + identityTransform
@@ -71,13 +100,29 @@ generateRay (Camera t _ _ _ ) (Ray o r) = Ray o (t * r)
 -- rayCast :: Ray -> Shape -> Bool
 -- rayCast r (Sphere Radius Point)
 
-saveImage :: String -> IO ()
+raster :: Image -> String
+raster (Image width height bitmap) = join "\n" line_list
+                                     where line_list = ["PS3", show width ++ " " ++ show height,
+                                                        "255", map (join " " . map show) bitmap]
+
+saveImage :: Image -> IO ()
 saveImage img = do
-   writeFile "image.pgm" img
+   writeFile "image.pgm" (raster img)
+
+sayHi :: IO ()
+sayHi = do
+  putStrLn "Hi!!"
 
 main :: IO ()
 main = do
-  putStrLn "Hello"
+  shared <- atomically $ newTVar 0
+  a <- forkIO $ sayHi 
+  b <- forkIO $ sayHi
+  dispVar shared
+  return ()
+
+atomRead = atomically . readTVar
+dispVar x = atomRead x >>= print
 
 --dot (Vector x1 y1 z1) (Vector x2 y2 z2) = x1*x2 + y1*y2 + z1*z2
 
