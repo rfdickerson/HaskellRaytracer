bc69f3548ade970b10254bde3dcd7784300874a2
Robert F. Dickerson <rfdickerson@gmail.com>
2015-11-15 18:34:20 -0600

    Ignoring the image that's produced.

--- src/Main.hs
+++ src/Main.hs
@@ -52,17 +52,14 @@ dot v1 v2 = V.sum $ V.map (\(x,y) -> x*y) (V.zip a b)
               a = M.getRow 1 v1
               b = M.getRow 1 v2
 
-screenWidth :: RealNum
-screenWidth = 800
+screenWidth :: Int
+screenWidth = 300
 
-screenHeight :: RealNum
-screenHeight = 600
+screenHeight :: Int
+screenHeight = 250
 
 maxRaySteps :: Integer
 maxRaySteps = 5
-
-newVector :: [RealNum] -> Point
-newVector x = M.fromList 4 1 x
 
 defaultCamera = Camera near far
   where
@@ -73,16 +70,26 @@ defaultCamera = Camera near far
 buildVector x y z = M.fromList 4 1 [x,y,z,0]
 
 defaultSphere :: Geometry
-defaultSphere = Sphere 1.0 (buildVector (-5) 0 0)
+defaultSphere = Sphere 0.05 (buildVector 5 0 0)
 
 -- | cameraTranform builds a projection matrix (Camera to World) from a Camera
-cameraTransform :: Camera -> Transform
-cameraTransform (Camera hither yon) = M.fromList 4 4 [0,0,0,0,0,0,0,0,0,0,a,b,0,0,0,0] + identityTransform
-  where a = yon / (yon-hither)
-        b = -(yon*hither/(yon-hither))
+perspective :: RealNum -> RealNum -> RealNum -> Transform
+perspective fov n f = eyeCoordScale fov * perspectMatrix
+  where
+    perspectMatrix = M.fromList 4 4 [0,0,0,0,0,0,0,0,0,0,a,b,0,0,0,0] + identityTransform
+    a = f*invDenom
+    b = -f*n*invDenom
+    invDenom = 1.0/(f-n)
+
+-- | Scales to canonical viewing volume
+eyeCoordScale :: RealNum -> Transform
+eyeCoordScale fov = scaleT v
+  where
+    v = buildVector i i 1
+    i = invTanAng fov
 
 invTanAng :: RealNum -> RealNum
-invTanAng fov = 1.0/tan(toRadians(fov) / 2.0)
+invTanAng fov = 1.0 / tan(toRadians fov  / 2.0)
 
 identityTransform :: Transform
 identityTransform = M.identity 4
@@ -95,13 +102,15 @@ scaleT v = M.fromList 4 4 [x,0,0,0,0,y,0,0,0,0,z,0,0,0,0,1]
              z = M.getElem 3 1 v
 
 translateT :: Vector -> Transform
-translateT v = identityTransform + (M.fromList 4 4 t)
+translateT v = identityTransform + M.fromList 4 4 t
   where
     t = [0,0,0] ++ [M.getElem 1 1 v]
       ++ [0,0,0] ++ [M.getElem 2 1 v]
       ++ [0,0,0] ++ [M.getElem 3 1 v]
 
 -- samples = [M.fromList 4 1 [x, y, 0, 0] | x <- [0..screenWidth], y <- [0..screenHeight]]
+
+-- | Squares a vector
 squareVector :: Vector -> RealNum
 squareVector v = M.getElem 1 1 (M.transpose a * a)
 
@@ -112,10 +121,7 @@ quadratic a b c =
 
 intersects :: Ray -> Geometry -> Bool
 intersects (Ray o v) (Sphere radius sposition) =
-  if discriminant >= 0 then
-    True
-  else
-    False
+  discriminant >= 0
   where
     discriminant = quadratic a b c
     a = squareVector v
@@ -125,9 +131,10 @@ intersects (Ray o v) (Sphere radius sposition) =
 
 rayTrace :: Int -> Int -> PixelRGB8
 rayTrace x y =
-  case intersects r defaultSphere of
-    True -> PixelRGB8 0 0 128
-    False -> PixelRGB8 33 33 33
+  if intersects r defaultSphere then
+    PixelRGB8 0 0 128
+  else
+    PixelRGB8 33 33 33
   where
     r = generateRay x y defaultCamera
 
@@ -136,29 +143,11 @@ generateRay :: Int -> Int -> Camera -> Ray
 generateRay x y c = Ray origin direction
   where
     origin = buildVector 0 0 0
-    direction = (cameraTransform c) * (buildVector fx fy 0.0)
-    fx = fromIntegral x
-    fy = fromIntegral y
+    direction = perspective 1.5 0.01 100.0  * buildVector fx fy 0.0
+    fx = -1 + 2 * (fromIntegral x / fromIntegral screenWidth)
+    fy = -1 + 2 * (fromIntegral y / fromIntegral screenHeight)
 
-
--- rayCast :: Ray -> Shape -> Bool
--- rayCast r (Sphere Radius Point)
 
 main :: IO ()
 main =
-  writePng "image.png" $ generateImage rayTrace 250 300
-    -- where pixelRenderer x y = PixelRGB8 (fromIntegral x) (fromIntegral y) 128
-
--- dot (Vector x1 y1 z1) (Vector x2 y2 z2) = x1*x2 + y1*y2 + z1*z2
-
---cross :: Vector -> Vector -> Vector
---cross (Vector x1 y1 z1) (Vector x2 y2 z2) = Vector x y z
---                                            where
---                                              x = y1 * z2 - z1 * y2
---                                              y = z1 * x2 - x1 * z2
---                                              z = x1 * y2 - y1 * x2
-
-
--- intersects :: Ray -> Shape -> Bool
--- insersects (Circle Radius Point) =
---  let e1 = p2 - p1
+  writePng "image.png" $ generateImage rayTrace screenWidth screenHeight
