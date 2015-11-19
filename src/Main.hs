1b520305f81990b72b48ba36dbaf7e26f17091bf
Robert F. Dickerson <rfdickerson@gmail.com>
2015-11-18 19:03:29 -0600

    Have sphere intersection, but doesn't look right

--- src/Main.hs
+++ src/Main.hs
@@ -13,24 +13,18 @@ type Point = Vector
 type Radius = RealNum
 type Transform = M.Matrix RealNum
 
+yellowPixel = PixelRGB8 255 215 0
+blackPixel = PixelRGB8 33 33 33
+
 -- | Ray Starting vector, Direction
 data Ray = Ray Point Vector deriving (Show, Eq)
 
 data Geometry = Sphere Radius Point
 
--- | Pixel represents a color, r, g, b
--- data Pixel = RGB Float Float Float deriving (Show, Eq)
-
-
 -- | Camera
 -- World to camera, cameraToWorld, clipHither, clipYon
 data Camera = Camera RealNum RealNum
   deriving (Show)
-
---pi :: RealNum
---pi = 3.14159265359
-
-
 
 rayEpsilon :: RealNum
 rayEpsilon = 1e-7
@@ -43,20 +37,20 @@ toRadians deg = deg * pi / 180.0
 -- | Vector length (norm)
 vlength :: Vector -> RealNum
 vlength v = sqrt (sumSqrs v)
-                         where sumSqrs m = V.foldl (\x y -> x**2 + y) 0 (M.getRow 1 m)
+  where sumSqrs m = V.foldl (\x y -> x**2 + y) 0 (M.getRow 1 m)
 
 -- | Dot product of vectors
 dot :: Vector -> Vector -> RealNum
 dot v1 v2 = V.sum $ V.map (\(x,y) -> x*y) (V.zip a b)
-            where
-              a = M.getRow 1 v1
-              b = M.getRow 1 v2
+  where
+    a = M.getRow 1 v1
+    b = M.getRow 1 v2
 
 screenWidth :: Int
-screenWidth = 300
+screenWidth = 200
 
 screenHeight :: Int
-screenHeight = 250
+screenHeight = 150
 
 maxRaySteps :: Integer
 maxRaySteps = 5
@@ -64,21 +58,25 @@ maxRaySteps = 5
 defaultCamera = Camera near far
   where
         near = 0.01
-        far = 100.0
+        far = 200.0
 
 -- | builds a new vector in homogeneous coordinates
-buildVector x y z = M.fromList 4 1 [x,y,z,0]
+buildVector x y z = M.fromList 4 1 [x,y,z,1]
 
 defaultSphere :: Geometry
-defaultSphere = Sphere 0.05 (buildVector 5 0 0)
+defaultSphere = Sphere 3.0 (buildVector 10 0 0)
 
 -- | cameraTranform builds a projection matrix (Camera to World) from a Camera
 perspective :: RealNum -> RealNum -> RealNum -> Transform
-perspective fov n f = eyeCoordScale fov * perspectMatrix
+perspective fov n f = perspectMatrix * eyeCoordScale fov
   where
-    perspectMatrix = M.fromList 4 4 [0,0,0,0,0,0,0,0,0,0,a,b,0,0,0,0] + identityTransform
+    perspectMatrix = m
+    m = M.fromList 4 4 [1,0,0,0,
+                        0,1,0,0,
+                        0,0,a,b,
+                        0,0,1,0]
     a = f*invDenom
-    b = -f*n*invDenom
+    b = (-f)*n*invDenom
     invDenom = 1.0/(f-n)
 
 -- | Scales to canonical viewing volume
@@ -94,19 +92,36 @@ invTanAng fov = 1.0 / tan(toRadians fov  / 2.0)
 identityTransform :: Transform
 identityTransform = M.identity 4
 
+normalize :: Vector -> Vector
+normalize v = M.fromList 4 1 [x/l, y/l, z/l, 1]
+  where
+  x = M.getElem 1 1 v
+  y = M.getElem 2 1 v
+  z = M.getElem 3 1 v
+  w = M.getElem 4 1 v
+  l = sqrt (x**2 + y**2 + z**2 + w**2)
+
+
 scaleT :: Vector -> Transform
-scaleT v = M.fromList 4 4 [x,0,0,0,0,y,0,0,0,0,z,0,0,0,0,1]
+scaleT v = M.fromList 4 4 [x,0,0,0,
+                           0,y,0,0,
+                           0,0,z,0,
+                           0,0,0,1]
            where
              x = M.getElem 1 1 v
              y = M.getElem 2 1 v
              z = M.getElem 3 1 v
 
 translateT :: Vector -> Transform
-translateT v = identityTransform + M.fromList 4 4 t
+translateT v = m + identityTransform
   where
-    t = [0,0,0] ++ [M.getElem 1 1 v]
-      ++ [0,0,0] ++ [M.getElem 2 1 v]
-      ++ [0,0,0] ++ [M.getElem 3 1 v]
+    m = M.fromList 4 4 [0,0,0,x,
+                        0,0,0,y,
+                        0,0,0,z,
+                        0,0,0,0]
+    x = M.getElem 1 1 v
+    y = M.getElem 2 1 v
+    z = M.getElem 3 1 v
 
 -- samples = [M.fromList 4 1 [x, y, 0, 0] | x <- [0..screenWidth], y <- [0..screenHeight]]
 
@@ -114,40 +129,53 @@ translateT v = identityTransform + M.fromList 4 4 t
 squareVector :: Vector -> RealNum
 squareVector v = M.getElem 1 1 (M.transpose a * a)
 
-
 quadratic :: RealNum -> RealNum -> RealNum -> RealNum
 quadratic a b c =
   b**2 - 4*a*c
 
+
+
 intersects :: Ray -> Geometry -> Bool
-intersects (Ray o v) (Sphere radius sposition) =
+intersects (Ray o v) (Sphere radius p) =
   discriminant >= 0
   where
     discriminant = quadratic a b c
-    a = squareVector v
-    b = 2 * dot v sposition
-    c = squareVector sposition - radius * 2
+    a = dot v v
+    b = 2 * dot os v
+    c = dot os os - radius * 2
+    os = p - o
 
+distance :: Ray -> Geometry -> RealNum
+distance (Ray o v) (Sphere radius p)
+  = (-b + sqrt discriminant) / (2*a)
+  where
+    discriminant = quadratic a b c
+    a = dot v v
+    b = 2 * dot os v
+    c = dot os os - radius ** 2
+    os = p - o
 
 rayTrace :: Int -> Int -> PixelRGB8
 rayTrace x y =
   if intersects r defaultSphere then
-    PixelRGB8 0 0 128
+    PixelRGB8 (round (5*d)) 128 0
   else
-    PixelRGB8 33 33 33
+    blackPixel
   where
     r = generateRay x y defaultCamera
-
+    d = distance r defaultSphere
 
 generateRay :: Int -> Int -> Camera -> Ray
 generateRay x y c = Ray origin direction
   where
     origin = buildVector 0 0 0
-    direction = perspective 1.5 0.01 100.0  * buildVector fx fy 0.0
-    fx = -1 + 2 * (fromIntegral x / fromIntegral screenWidth)
-    fy = -1 + 2 * (fromIntegral y / fromIntegral screenHeight)
+    direction = normalize $ perspective 55.0 0.1 50.0  * buildVector ndx ndy 0
+    --ndx = 2*(fromIntegral x / fromIntegral screenWidth) - 1
+    --ndy = 2*(fromIntegral y / fromIntegral screenHeight) - 1
+    ndx = (fromIntegral x / fromIntegral screenWidth)
+    ndy = (fromIntegral y / fromIntegral screenHeight)
 
 
 main :: IO ()
 main =
-  writePng "image.png" $ generateImage rayTrace screenWidth screenHeight
+  writePng "image.png" ( generateImage rayTrace screenWidth screenHeight)
